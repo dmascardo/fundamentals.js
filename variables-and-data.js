ee4848f0a370360fe5594019c15a7c5c35b37d4c
Denise Rose <dmascardo@cnm.edu>
2025-09-04 05:55:02 -0600

    let wordData (5:48)

---
+++ variables-and-data.js
@@ -0,0 +1,13 @@
+/*
+* Variables, Data Types, and Typing
+ */
+ let wordData
+
+wordData = "Word list";
+
+ console.log(wordData);
+
+ let subTotal = 100;
+ let tax = subTotal * 0.07;
+ let totalWithTax = tax + subTotal;
+
