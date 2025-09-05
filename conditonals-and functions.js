c135f9f53c79730f60a26d9541565975e8addbd7
Denise Rose <dmascardo@cnm.edu>
2025-09-05 07:42:26 -0600

    continuing array

--- conditonals-and functions.js
+++ conditonals-and functions.js
@@ -20,3 +20,48 @@ let lessThanEq = 4 <= 9;
 
 // Not Equals
 let notEquals = 5 !== 2;
+
+
+let storeA = 1.40;
+let storeB = 3.40;
+
+let storeAIsLower = storeA <  storeB;
+
+function compareStorePrices (storeA, storeB) {
+    if (storeAIsLower) {
+    console.log("Store A has a lower price.");
+    } else if (storeB < storeA) {
+    console.log("Store B has a lower price.");
+    } else  {
+    console.log("Their prices are equal.")
+    }
+}
+
+//compareStorePrices(10, 5);
+//compareStorePrices(7, 10);
+
+function squareNum (number) {
+    let squared = number * number;
+    return squared;p
+}
+let squareNumber = squareNum(7) ;
+console.log(squareNumber);
+
+let x = 10;
+
+    function addNumber(n, m, x) {
+        console.log(x);
+        let b;
+        if (1==1) {
+            let b = 8;
+        }
+        console.log(b);
+         return n + m;
+}
+
+
+
+
+//                       0   1  2  3  4
+let ourArray = [1, 2, 3, 4, 5];
+    for(counter; comp; incr; )
